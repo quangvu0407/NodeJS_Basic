da67b6d5de1968be6bab11998685ef22b16a80b2
quangvu0407 <trantuyenquang0407@gmail.com>
2025-11-07 23:18:57 +0700

    handling error middleware

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,23 +5,20 @@
  */
 
 import { StatusCodes } from 'http-status-codes'
+// import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body', req.body)
-    console.log('req.query', req.query)
-    console.log('req.params', req.params)
-    console.log('req.files', req.files)
-    console.log('req.cookies', req.cookies)
-    console.log('req.jwtDecoded', req.jwtDecoded)
+    // console.log('req.body', req.body)
+    // console.log('req.query', req.query)
+    // console.log('req.params', req.params)
+    // console.log('req.files', req.files)
+    // console.log('req.cookies', req.cookies)
+    // console.log('req.jwtDecoded', req.jwtDecoded)
 
-    res.status(StatusCodes.CREATED).json({ message: 'POST from controller: API create new board' })
+    // res.status(StatusCodes.CREATED).json({ message: 'POST from controller: API create new board' })
   }
-  catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  catch (error) { next(error) }
 }
 
 export const boardController = {
