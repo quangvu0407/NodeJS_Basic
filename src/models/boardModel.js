7b8d522a3944c566cbb05d401b99a6d20ff2e90a
quangvu0407 <trantuyenquang0407@gmail.com>
2025-11-16 17:20:17 +0700

    model board column card

---
+++ src/models/boardModel.js
@@ -0,0 +1,20 @@
+import Joi from 'joi'
+
+//Define collection (Name && Schema)
+
+const BOAD_COLLECTION_NAME = 'boards'
+const BOAD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(255).trim().strict(),
+
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const boardModel = {
+  BOAD_COLLECTION_NAME,
+  BOAD_COLLECTION_SCHEMA
+}
---
+++ src/utils/validators.js
@@ -0,0 +1,5 @@
+
+// validation data sang kieu object string
+
+export const OBJECT_ID_RULE = /^[0-9a-fA-F]{24}$/
+export const OBJECT_ID_RULE_MESSAGE = 'Your string fails to match the Object  Id pattern!'
