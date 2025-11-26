9504dba74bfa07a43f32760e988201b74aea7962
quangvu0407 <trantuyenquang0407@gmail.com>
2025-11-26 15:32:05 +0700

    create new board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatter'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -9,8 +10,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
+    //Gọi tới tầng model để xử lý lưu bản ghi newBoard vào trong db
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+
+    // lấy bản ghi board sau khi gọi tùy dự án có cần bước này hay ko
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
+
     //Tra ket qua ve, trong service luon phai co return.
-    return newBoard
+    return getNewBoard
   }
   catch (error) { throw (error) }
 }
